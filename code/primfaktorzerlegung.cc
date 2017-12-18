405befdb48a5622cc5a4f31d32136abb99c9d72e
drb3b3 <silvan@zingrebe.com>
2017-12-18 21:04:57 +0100

    Added the actual code

---
+++ code/primfaktorzerlegung.cc
@@ -0,0 +1,55 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+size_t teile(size_t zahl)
+{
+	size_t teiler = 2;
+	size_t x = sqrt(zahl);
+	while (zahl % teiler != 0)
+	{
+		if (teiler >= x)
+		{
+			return zahl;
+		}
+		else
+		{
+			++teiler;
+		}
+	}
+
+	return teiler;
+}
+
+std::string primfaktorzerlegung(size_t zahl)
+{
+	std::stringstream ss;
+	size_t teiler = 0;
+
+	ss << zahl << " = ";
+
+	while (zahl != 1)
+	{
+		teiler = teile(zahl);
+		ss << teiler;
+		zahl = zahl / teiler;
+		if (zahl != 1)
+			ss << "*";
+	}
+
+	return ss.str();
+}
+
+int main(  )
+{
+	std::cout << primfaktorzerlegung(99) << std::endl;
+	for (size_t i = 3; i < 0xFFFFFFFFFFFFFFFF; ++i)
+	{
+		std::cout << primfaktorzerlegung(i) << std::endl;
+	}
+
+	system("pause");
+
+    return 0;
+}
+
